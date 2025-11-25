d6f3ed9afb512f29742addfde5ca4cdd933b985d
Serhii Popov <serhii.popov@codeit.pro>
2025-11-25 16:47:04 +0100

    components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="text-red-500">hello world</div>;
+  return (
+    <div className="text-red-500">
+      <Button>Click me</Button>
+    </div>
+  );
 }
