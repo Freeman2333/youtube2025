b7a175e2ecedc538b7183a9e9c76ac35a2bfdd70
Serhii Popov <serhii.popov@codeit.pro>
2025-11-27 22:16:59 +0100

    db and drizzle added

---
+++ src/db/schema.ts
@@ -0,0 +1,24 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "user",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").notNull().unique(),
+
+    name: text("name").notNull(),
+    imageUrl: text("image_url").notNull(),
+
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt")
+      .notNull()
+      .$onUpdate(() => new Date()),
+  },
+  (user) => [uniqueIndex("clerk_id_idx").on(user.clerkId)]
+);
